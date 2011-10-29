53dfca03d15b38af4def0539287565c79450d815
federico.e.rossi@gmail.com <federico.e.rossi@gmail.com@b25e7df3-8006-126a-9efd-0d622fa96d20>
2011-10-29 14:27:44 +0000

    Tarea: Crear script ejecutable para windows. Estado/Descripcion: Completa. Construido el .bat para ejecutar la aplicacion en Windows. Conteniendo como comentario el codigo perteneciente a Linux.

---
+++ trunk/bin/scheduling_simulator.bat
@@ -0,0 +1,6 @@
+//#!/bin/bash
+//export CLASSPATH=$CLASSPATH:.
+//java -cp ./scheduling_simulator.jar Controller.SchedulingSystem.SchedulingSystem
+set CLASSPATH=CLASSPATH:.
+java -cp ./scheduling_simulator.jar Controller.SchedulingSystem.SchedulingSystem
+PAUSE
