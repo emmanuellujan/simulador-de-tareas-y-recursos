273afcaa035f9afca6742aab3ba3c8ac88c016b7
federico.e.rossi <federico.e.rossi@gmail.com>
2011-11-02 02:07:48 +0000

    Evaluacion componentes grafo: Nuevas clases encargadas de analizar el grafico y evaluar diferentes conceptos para recuperar elementos del mismo que se identifiquen de acuerdo a la busqueda realizada.

---
+++ src/Controller/FilterSystem/SimpleFilter.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller.FilterSystem;
+
+/**
+ *
+ * @author F.Rossi
+ */
+public abstract class SimpleFilter extends Filter{
+    
+    protected Object element;
+    
+    public SimpleFilter(Object obj){
+        this.setElement(obj);
+    }
+    
+    protected Object getElement() {
+        return element;
+    }
+
+    protected void setElement(Object elem) {
+	this.element = elem;
+    }
+    
+}
