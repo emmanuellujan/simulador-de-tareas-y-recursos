d695c7b88aa7ef378fb505a1f683540d5f99dada
federico.e.rossi@gmail.com <federico.e.rossi@gmail.com@b25e7df3-8006-126a-9efd-0d622fa96d20>
2011-11-14 23:24:40 +0000

    Creación interfaz gráfica: Modificacion de los frames de actores y artefactos.

--- trunk/src/Gui/Frames/CreateActorFrame.java
+++ trunk/src/Gui/Frames/CreateActorFrame.java
@@ -10,15 +10,29 @@
  */
 package Gui.Frames;
 
+import Controller.SchedulingAlgorithmSystem.FCFS;
+import Controller.SchedulingAlgorithmSystem.PrioritiesSA;
+import Controller.SchedulingAlgorithmSystem.SchedulingAlgorithm;
+import Controller.SchedulingSystem.Actor;
+import Controller.SchedulingSystem.SchedulingSystem;
+import java.util.Hashtable;
+import java.util.Vector;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
  * @author F.Rossi
  */
+//CHECKEAR SCHEDULING SYSTEM EN ENTRADA, FUNCION EN DESARROLLO Y EN CERRAR VENTANA CON CREACION
+//COMO HAGO PARA MODIFICAR SIEMPRE EL MISMO MAIN FRAME??
 public class CreateActorFrame extends javax.swing.JFrame {
 
-    /** Creates new form CreateActorFrame */
-    public CreateActorFrame() {
-        initComponents();
+    /** 
+     * Creates new form CreateActorFrame 
+     */
+    private CreateActorFrame() {
+        initComponents();        
     }
 
     /** This method is called from within the constructor to
@@ -38,15 +52,14 @@ public class CreateActorFrame extends javax.swing.JFrame {
         jPanel2 = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
-        jPanel3 = new javax.swing.JPanel();
-        jScrollPane2 = new javax.swing.JScrollPane();
-        jList1 = new javax.swing.JList();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
         jTextField1 = new javax.swing.JTextField();
         jTextField2 = new javax.swing.JTextField();
         jComboBox1 = new javax.swing.JComboBox();
         jTextField3 = new javax.swing.JTextField();
+        jLabel5 = new javax.swing.JLabel();
+        jTextField4 = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -64,6 +77,22 @@ public class CreateActorFrame extends javax.swing.JFrame {
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null},
                 {null, null},
                 {null, null},
                 {null, null},
@@ -92,28 +121,12 @@ public class CreateActorFrame extends javax.swing.JFrame {
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
-        jPanel3.setBorder(javax.swing.BorderFactory.createTitledBorder("Relations"));
-
-        jScrollPane2.setViewportView(jList1);
-
-        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
-        jPanel3.setLayout(jPanel3Layout);
-        jPanel3Layout.setHorizontalGroup(
-            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel3Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 375, Short.MAX_VALUE)
-                .addContainerGap())
-        );
-        jPanel3Layout.setVerticalGroup(
-            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel3Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
-
         jButton1.setText("Create");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         jButton2.setText("Cancel");
         jButton2.addActionListener(new java.awt.event.ActionListener() {
@@ -122,11 +135,15 @@ public class CreateActorFrame extends javax.swing.JFrame {
             }
         });
 
+        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "First come first server", "Prioridades" }));
+
         jTextField3.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jTextField3ActionPerformed(evt);
             }
         });
+
+        jLabel5.setText("Maximum number of relations");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -135,28 +152,29 @@ public class CreateActorFrame extends javax.swing.JFrame {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                        .addComponent(jButton1)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jButton2)
-                        .addGap(17, 17, 17))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                    .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(jPanel2, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jPanel3, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                             .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(jLabel1)
-                                    .addComponent(jLabel2)
-                                    .addComponent(jLabel3)
-                                    .addComponent(jLabel4))
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 152, Short.MAX_VALUE)
-                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                    .addComponent(jTextField3)
-                                    .addComponent(jComboBox1, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                    .addComponent(jTextField2)
-                                    .addComponent(jTextField1, javax.swing.GroupLayout.DEFAULT_SIZE, 131, Short.MAX_VALUE))))
-                        .addGap(18, 18, 18))))
+                                .addComponent(jButton1)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jButton2))
+                            .addComponent(jPanel2, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addGap(10, 10, 10))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel1)
+                            .addComponent(jLabel2)
+                            .addComponent(jLabel3)
+                            .addComponent(jLabel4)
+                            .addComponent(jLabel5))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 125, Short.MAX_VALUE)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jTextField4)
+                            .addComponent(jTextField3)
+                            .addComponent(jComboBox1, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jTextField2)
+                            .addComponent(jTextField1, javax.swing.GroupLayout.DEFAULT_SIZE, 131, Short.MAX_VALUE))
+                        .addGap(21, 21, 21))))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -178,13 +196,16 @@ public class CreateActorFrame extends javax.swing.JFrame {
                     .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel4))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel5)
+                    .addComponent(jTextField4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(13, 13, 13)
                 .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButton2)
-                    .addComponent(jButton1)))
+                    .addComponent(jButton1))
+                .addGap(158, 158, 158))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -193,14 +214,14 @@ public class CreateActorFrame extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 436, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 345, Short.MAX_VALUE)
                 .addContainerGap())
         );
 
@@ -212,8 +233,102 @@ public class CreateActorFrame extends javax.swing.JFrame {
     }//GEN-LAST:event_jTextField3ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        SimulatorFrame.getInstance().setActorsList(this.getActorsList());
         this.setVisible(false);
     }//GEN-LAST:event_jButton2ActionPerformed
+
+    /*
+     * Action of 'Create' button, which creates a new Actor taking the values
+     * of the screen components. Updating Actors list too, and closing the
+     * current panel.
+     */
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        if(this.areValidFields()){ 
+            //Get selected algorithm        
+            SchedulingAlgorithm saReadyList;
+            if (this.jComboBox1.getSelectedItem().equals("Prioridades")) {
+                saReadyList = new PrioritiesSA();
+            } else {
+                saReadyList = new FCFS();
+            }
+
+            /*Get Properties*/
+            Hashtable propertiesTable = new Hashtable();
+
+            for (int i = 0; i < this.jTable1.getRowCount(); i++) {
+                if ((this.jTable1.getValueAt(i, 0) != null) && (this.jTable1.getValueAt(i, 1) != null)) {
+                    propertiesTable.put(this.jTable1.getValueAt(i, 0).toString(), this.jTable1.getValueAt(i, 1).toString());
+                } else {
+                    i = this.jTable1.getRowCount();
+                }
+            }
+
+            this.newActor = new Actor("actor" + String.valueOf(this.getActorsList().size()), saReadyList, Integer.parseInt(this.jTextField3.getText()),
+                    this.getSchedulingSystem(), Integer.parseInt(this.jTextField1.getText()), Integer.parseInt(this.jTextField2.getText()),
+                    propertiesTable, Integer.parseInt(this.jTextField4.getText()), null);
+            this.getActorsList().add(this.newActor);
+            SimulatorFrame.getInstance().setActorsList(this.getActorsList());        
+            this.setVisible(false);
+            this.setFieldsValuesEmpty();
+        }else{
+            ErrorFrame.getInstance().setLabel("Wrong inserted values in fields");
+            ErrorFrame.getInstance().setVisible(true);
+        }
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private boolean areValidFields(){
+        if(!isNum(this.jTextField1.getText()))//Capacity
+            return false;
+        if(!isNum(this.jTextField2.getText()))//MaxTasks
+            return false;
+        if(!isNum(this.jTextField3.getText()))//Quantum
+            return false; 
+        if(!isNum(this.jTextField4.getText()))//MaxRelations
+            return false;        
+        
+        return true;    
+    }
+    
+    public static boolean isNum(String s) {
+        try {
+            Integer.parseInt(s);
+        }
+        catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
+    }
+    
+    private void setFieldsValuesEmpty(){
+        this.jTextField1.setText(null);
+        this.jTextField2.setText(null);
+        this.jTextField3.setText(null);
+        this.jTextField4.setText(null);
+        this.clearTable();
+    }
+    
+    private void clearTable(){
+        for (int i = 0; i < this.jTable1.getRowCount(); i++)
+          for(int j = 0; j < this.jTable1.getColumnCount(); j++) {
+              this.jTable1.setValueAt("", i, j);
+          }        
+    }
+    
+    public void setSchedulingSystem(SchedulingSystem schedulingSystem) {
+        this.currentSchedulingSystem = schedulingSystem;
+    }
+
+    public SchedulingSystem getSchedulingSystem() {
+        return this.currentSchedulingSystem;
+    }
+
+    public Vector<Actor> getActorsList() {
+        return actorsList;
+    }
+
+    public void setActorsList(Vector<Actor> actorsList) {
+        this.actorsList = actorsList;
+    }
 
     /**
      * @param args the command line arguments
@@ -246,10 +361,21 @@ public class CreateActorFrame extends javax.swing.JFrame {
         java.awt.EventQueue.invokeLater(new Runnable() {
 
             public void run() {
-                new CreateActorFrame().setVisible(true);
+                //new CreateActorFrame().setVisible(true);
             }
         });
     }
+
+    public static CreateActorFrame getInstance() {
+        if (CREATEACTORFRAME_INSTANCE == null) {
+            CREATEACTORFRAME_INSTANCE = new CreateActorFrame();
+        }
+        return CREATEACTORFRAME_INSTANCE;
+    }
+    private Actor newActor;
+    private SchedulingSystem currentSchedulingSystem;
+    private Vector<Actor> actorsList;
+    private static CreateActorFrame CREATEACTORFRAME_INSTANCE;
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
@@ -258,15 +384,14 @@ public class CreateActorFrame extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
-    private javax.swing.JList jList1;
+    private javax.swing.JLabel jLabel5;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
-    private javax.swing.JPanel jPanel3;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JTable jTable1;
     private javax.swing.JTextField jTextField1;
     private javax.swing.JTextField jTextField2;
     private javax.swing.JTextField jTextField3;
+    private javax.swing.JTextField jTextField4;
     // End of variables declaration//GEN-END:variables
 }
