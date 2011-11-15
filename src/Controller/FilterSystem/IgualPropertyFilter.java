27806825d2b3c0d9386482b057f6b4a6c74b4904
federico.e.rossi <federico.e.rossi@gmail.com>
2011-11-15 00:39:34 +0000

    Creación interfaz gráfica: Incorporados nuevos filtros de propiedades para la simulacion.

---
+++ src/Controller/FilterSystem/IgualPropertyFilter.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller.FilterSystem;
+
+import Controller.SchedulingSystem.Resource;
+
+/**
+ *
+ * @author F.Rossi
+ */
+public class IgualPropertyFilter extends PropertyFilter{
+    
+    public IgualPropertyFilter(Object currentKey, Object currentValue){
+        super(currentKey,currentValue);
+    }
+
+    @Override
+    public boolean eval(Resource a) {
+        return  (a.getProperty(this.getKey().toString()).equals(this.getValue()));
+    }
+    
+}
