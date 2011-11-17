b8262d27e9dcbf636d1099708bc7213399a156aa
federico.e.rossi@gmail.com <federico.e.rossi@gmail.com@b25e7df3-8006-126a-9efd-0d622fa96d20>
2011-11-17 15:44:53 +0000

    Creación interfaz gráfica: Se realizaron numerosas modificaciones en la estructura de la creacion de tareas, incorporando nuevas clases y nuevos metodos.

--- trunk/src/Gui/Frames/UpdaterFrame.java
+++ trunk/src/Gui/Frames/UpdaterFrame.java
@@ -10,6 +10,13 @@
  */
 package Gui.Frames;
 
+import Controller.FilterSystem.Filter;
+import Controller.SchedulingSystem.Resource;
+import Controller.SchedulingSystem.Update;
+import Controller.SchedulingSystem.Updater;
+import java.util.Hashtable;
+import java.util.Vector;
+
 /**
  *
  * @author F.Rossi
@@ -30,21 +37,155 @@ public class UpdaterFrame extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jButton3 = new javax.swing.JButton();
+        jComboBox1 = new javax.swing.JComboBox();
+        jLabel2 = new javax.swing.JLabel();
+        jButton4 = new javax.swing.JButton();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Updater configuration", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Verdana", 1, 11))); // NOI18N
+
+        jLabel1.setText("Add filter");
+
+        jButton3.setText("Create");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+
+        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Element Equal Filter", "Job Position Filter", "List Property Filter" }));
+
+        jLabel2.setText("Include related update");
+
+        jButton4.setText("Create");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel2)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 114, Short.MAX_VALUE)
+                        .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jButton3, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jButton4, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap())
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(jButton3)
+                    .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(jButton4))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        jButton1.setText("Cancel");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jButton2.setText("Create");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(jButton2)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jButton1)))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton1)
+                    .addComponent(jButton2))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        this.setVisible(false);
+        ((CreateTaskFrame)this.getBackFrame()).setLocationRelativeTo(null);
+        ((CreateTaskFrame)this.getBackFrame()).setVisible(true);
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        UpdateFrame updateFrame = new UpdateFrame();
+        updateFrame.setBackFrame(this);
+        this.setVisible(false);
+        updateFrame.setLocationRelativeTo(null);
+        updateFrame.main(null);
+        updateFrame.setVisible(true);
+    }//GEN-LAST:event_jButton4ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        CreateFilterFrame filterFrame = new CreateFilterFrame();
+        filterFrame.setFilterType((String)this.jComboBox1.getSelectedItem());
+        filterFrame.setBackFrame("UpdaterFrame", this);
+        filterFrame.setResourcesList(this.getResourcesList());
+        filterFrame.setMainResourcesList(this.getMainResourcesList());
+        filterFrame.setFilterFrameStateI(false);
+        filterFrame.setFilterFrameStateII(false);
+        this.setVisible(false);
+        filterFrame.setLocationRelativeTo(null);
+        filterFrame.main(null);
+        filterFrame.setVisible(true);
+    }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        Updater currentUpdater = this.getUpdater();
+        if((this.getFilter() != null)&&(this.getUpdate() != null))
+            currentUpdater.getUpdates().put(this.getFilter(), this.getUpdate());       
+        this.setUpdater(currentUpdater);                
+        ((CreateTaskFrame)this.getBackFrame()).setUpdater(currentUpdater);
+        this.setVisible(false);
+        ((CreateTaskFrame)this.getBackFrame()).setLocationRelativeTo(null);
+        ((CreateTaskFrame)this.getBackFrame()).setVisible(true);
+        
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -77,21 +218,84 @@ public class UpdaterFrame extends javax.swing.JFrame {
         java.awt.EventQueue.invokeLater(new Runnable() {
 
             public void run() {
-                if (UPDATERFRAME == null)
-                    UPDATERFRAME = new UpdaterFrame();                        
-                UPDATERFRAME.setVisible(true);
+                if (this == null)
+                    new UpdaterFrame(); 
             }
         });
-    }    
-    
-    public static UpdaterFrame getInstance() {
-        if (UPDATERFRAME == null)
-            UPDATERFRAME = new UpdaterFrame();        
-        return UPDATERFRAME;
     }
     
-    private static UpdaterFrame UPDATERFRAME;
+    public Vector<Resource> getResourcesList() {
+        return resourcesList;
+    }
+
+    public void setResourcesList(Vector<Resource> resourcesList) {
+        this.resourcesList = resourcesList;      
+    }
+    
+    public void setMainResourcesList(Vector resourcesPrincipalList) {
+        this.resourcesMainList = resourcesPrincipalList;        
+    }
+    
+    public Vector getMainResourcesList() {
+        return this.resourcesMainList;        
+    }
+    
+    public void setBackFrame(Object backFrame) {
+        this.filterBackFrame = (CreateFilterFrame)backFrame;            
+    }
+
+    public Object getBackFrame() {
+       return this.filterBackFrame;            
+    }
+    
+    public Hashtable<Filter, Update> getUpdates() {
+	return updates;
+    }
+
+    public void setUpdates(Hashtable<Filter, Update> updates) {
+	this.updates = updates;
+    }
+    
+    public Filter getFilter() {
+	return this.filter;
+    }
+
+    public void setFilter(Filter filter) {
+	this.filter = filter;
+    }
+    
+    public Update getUpdate() {
+	return this.update;
+    }
+
+    public void setUpdate(Update update) {
+	this.update = update;
+    }
+    
+    public Updater getUpdater() {
+	return this.updater;
+    }
+    
+    public void setUpdater(Updater current) {
+	this.updater = current;
+    }
+
+    private CreateFilterFrame filterBackFrame;
+    private Hashtable<Filter,Update> updates;
+    private Vector resourcesMainList;// Lista total de recursos, artefactos y actores
+    private Vector<Resource> resourcesList; //Lista de artefactos
+    private Filter filter;
+    private Update update;
+    private Updater updater;
     
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JComboBox jComboBox1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
