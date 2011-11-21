c9b80d421f31fd70dfd0a54c785a48cebf5a12e0
lujan.emmanuel <lujan.emmanuel@gmail.com>
2011-11-21 18:52:14 +0000

    Avances en issue 25

--- src/Model/ResultsAnalyzer/ResultsAnalyzer.java
+++ src/Model/ResultsAnalyzer/ResultsAnalyzer.java
@@ -21,101 +21,121 @@ public class ResultsAnalyzer {
 	public float propFinishedTasks;
 	public float propFailedTasks;
 	public float propVelocity;
-	
+
 	private String analysis;
-	
-	public ResultsAnalyzer(SchedulingSystem schedulingSystem){
+
+	public ResultsAnalyzer(SchedulingSystem schedulingSystem) {
 		this.setSchedulingSystem(schedulingSystem);
 		this.setAnalysis("");
 	}
-	
+
 	public void analyze() {
 		SchedulingSystem schedulingSystem = this.getSchedulingSystem();
 		CompLogginSystem logger = schedulingSystem.getCompLogginSystem();
-		
-		int numberOfErrors=0;
-		if(logger.getErrorMsgs()!=null)
+
+		int numberOfErrors = 0;
+		if (logger.getErrorMsgs() != null)
 			numberOfErrors = logger.getErrorMsgs().size();
 		this.setNumberOfErrors(numberOfErrors);
-		
+
 		this.setDeadline(schedulingSystem.getDeadline());
-		
+
 		Vector<SimulationTime> simulationTimes = logger.getSimulationTimes();
 		this.setNumberOfCycles(simulationTimes.size());
-		
+
 		int nbrSuccessfulTasks = schedulingSystem.getFinishedList().size();
 		this.setNbrSuccessfulTasks(nbrSuccessfulTasks);
-		
+
 		int nbrFailedTasks = schedulingSystem.getFailedFinishedList().size();
 		this.setNbrFailedTasks(nbrFailedTasks);
-		
-		int numberOfTasks = nbrSuccessfulTasks+nbrFailedTasks;
+
+		int numberOfTasks = nbrSuccessfulTasks + nbrFailedTasks;
 		this.setNumberOfTasks(numberOfTasks);
-		
+
 		int numberOfActors = this.getSchedulingSystem().getActorsList().size();
 		this.setNumberOfActors(numberOfActors);
-		
-		int numberOfResources = this.getSchedulingSystem().getResourcesList().size();
+
+		int numberOfResources = this.getSchedulingSystem().getResourcesList()
+				.size();
 		this.setNumberOfResources(numberOfResources);
-		
-		float meanNbrTasksPerActor = (float) numberOfTasks / (float) numberOfActors;
+
+		float meanNbrTasksPerActor = (float) numberOfTasks
+				/ (float) numberOfActors;
 		this.setMeanNbrTasksPerActor(meanNbrTasksPerActor);
-		
-		float propFinishedTasks = (float) nbrSuccessfulTasks / (float) numberOfTasks;
+
+		float propFinishedTasks = (float) nbrSuccessfulTasks
+				/ (float) numberOfTasks;
 		this.setPropFinishedTasks(propFinishedTasks);
-		
+
 		float propFailedTasks = (float) nbrFailedTasks / (float) numberOfTasks;
 		this.setPropFailedTasks(propFailedTasks);
-		
-		float propVelocity = (float) this.getDeadline() / (float) this.getNumberOfCycles();
+
+		float propVelocity = (float) this.getDeadline()
+				/ (float) this.getNumberOfCycles();
 		this.setPropVelocity(propVelocity);
-		
+
 		this.renderAnalysis();
-	
+
 	}
-	
-	public void renderAnalysis(){
+
+	public void renderAnalysis() {
 		String analysis = this.getAnalysis();
+
+		analysis += "Results Analisys:\n";
 		
-		analysis+="Results Analisys:\n";
-		
-		analysis+="\tNumber of errors: "+this.getNumberOfErrors()+"\n";
-		
-		analysis+="\tDeadline (in cycles): "+this.getDeadline()+"\n";
-		
-		analysis+="\tNumber of cycles: "+this.getNumberOfCycles()+"\n";
-		
-		analysis+="\tTotal number of executed tasks, including contingency tasks: "+this.getNumberOfTasks()+"\n";
-		
-		analysis+="\tNumber of successful tasks, including contingency tasks: "+this.getNbrSuccessfulTasks()+"\n";
-		
-		analysis+="\tNumber of failed tasks: "+this.getNbrFailedTasks()+"\n";
-		
-		analysis+="\tNumber of actors: "+this.getNumberOfActors()+"\n";
-		
-		analysis+="\tNumber of other resources (artifacts): "+this.getNumberOfResources()+"\n";
-		
-		analysis+="\tMean number of tasks per actor: "+this.getMeanNbrTasksPerActor()+"\n";
-		
-		analysis+="\tProportions: \n";
-		
-		analysis+="\t\tSuccessful tasks proportion, including contingency tasks: "+this.getPropFinishedTasks()+"\n";
-		
-		analysis+="\t\tFailed tasks proportion, including contingency tasks: "+this.getPropFailedTasks()+"\n";
-		
-		analysis+="\t\tVelocity proportion (deadline/nbrOfCycles): "+this.getPropVelocity()+"\n";
-		
+		analysis += "\tNumber of errors: " + this.getNumberOfErrors() + "\n";
+
+		analysis += "\tDeadline (in cycles): " + this.getDeadline() + "\n";
+
+		analysis += "\tNumber of cycles: " + this.getNumberOfCycles() + "\n";
+
+		analysis += "\tTotal number of executed tasks, including contingency tasks: "
+				+ this.getNumberOfTasks() + "\n";
+
+		analysis += "\tNumber of successful tasks, including contingency tasks: "
+				+ this.getNbrSuccessfulTasks() + "\n";
+
+		analysis += "\tNumber of failed tasks: " + this.getNbrFailedTasks()
+				+ "\n";
+
+		analysis += "\tNumber of actors: " + this.getNumberOfActors() + "\n";
+
+		analysis += "\tNumber of other resources (artifacts): "
+				+ this.getNumberOfResources() + "\n";
+
+		analysis += "\tMean number of tasks per actor: "
+				+ this.getMeanNbrTasksPerActor() + "\n";
+
+		analysis += "\tProportions: \n";
+
+		analysis += "\t\tSuccessful tasks proportion, including contingency tasks: "
+				+ this.getPropFinishedTasks() + "\n";
+
+		analysis += "\t\tFailed tasks proportion, including contingency tasks: "
+				+ this.getPropFailedTasks() + "\n";
+
+		analysis += "\t\tVelocity proportion (deadline/nbrOfCycles): "
+				+ this.getPropVelocity() + "\n";
+
+		analysis += "\n";
+
+		analysis += this.getSchedulingSystem().getCompLogginSystem()
+				.renderErrors();
+
 		this.setAnalysis(analysis);
 	}
-	
+
 	public void print() {
 		System.out.println(this.getAnalysis());
 	}
-	
+
 	public void writeAnalysis() {
 		String analysis = this.getAnalysis();
-		String dir = this.getSchedulingSystem().getConfigurator().getOutputDir();
-		String outputFile = this.getSchedulingSystem().getConfigurator().getOutputFile()+"_analysis.txt";
+		String dir = this.getSchedulingSystem().getConfigurator()
+				.getOutputDir();
+		String outputFile = this.getSchedulingSystem().getConfigurator()
+				.getOutputFile()
+				+ "_analysis.txt";
 		String fileName = dir + outputFile;
 		FileManager fileManager = new FileManager();
 		fileManager.writeFile(fileName, analysis);
@@ -128,7 +148,7 @@ public class ResultsAnalyzer {
 	public void setSchedulingSystem(SchedulingSystem schedulingSystem) {
 		this.schedulingSystem = schedulingSystem;
 	}
-	
+
 	public int getDeadline() {
 		return deadline;
 	}
@@ -136,7 +156,7 @@ public class ResultsAnalyzer {
 	public void setDeadline(int deadline) {
 		this.deadline = deadline;
 	}
-	
+
 	public int getNumberOfErrors() {
 		return numberOfErrors;
 	}
@@ -196,7 +216,7 @@ public class ResultsAnalyzer {
 	public void setPropVelocity(float propVelocity) {
 		this.propVelocity = propVelocity;
 	}
-	
+
 	public float getPropVelocity() {
 		return this.propVelocity;
 	}
@@ -232,5 +252,5 @@ public class ResultsAnalyzer {
 	public void setAnalysis(String analysis) {
 		this.analysis = analysis;
 	}
-	
+
 }
