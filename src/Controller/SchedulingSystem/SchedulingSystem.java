260a803b826895539eac5919224d1c68a63a1b42
lujan.emmanuel <lujan.emmanuel@gmail.com>
2011-11-30 22:51:23 +0000

    Avances en la integración.

--- src/Controller/SchedulingSystem/SchedulingSystem.java
+++ src/Controller/SchedulingSystem/SchedulingSystem.java
@@ -211,9 +211,8 @@ public class SchedulingSystem {
 	}
 
 	public void saveData() {
-		// SerialIOSystem serialIOSystem = new
-		// SerialIOSystem(this.getConfigurator(), this);
-		// serialIOSystem.saveAll();
+		//SerialIOSystem serialIOSystem = new SerialIOSystem(this.getConfigurator(), this);
+		//serialIOSystem.saveAll();
 		this.getIoSystem().saveAll();
 		this.getCompLogginSystem().writeLog();
 		this.getResultsAnalyzer().writeAnalysis();
