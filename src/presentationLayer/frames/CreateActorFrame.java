f5e17cc6dff295188a497460c0576be14df5d099
federico.e.rossi <federico.e.rossi@gmail.com>
2011-12-17 22:26:29 +0000

    Actualizada la nueva ventana correspondiente a la creacion del Updater y sus correspondientes actualizaciones.

--- src/presentationLayer/frames/CreateActorFrame.java
+++ src/presentationLayer/frames/CreateActorFrame.java
@@ -279,7 +279,7 @@ public class CreateActorFrame extends javax.swing.JFrame {
 
         jLabel6.setText("Updater");
 
-        jButton3.setText("Create");
+        jButton3.setText("Edit");
         jButton3.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton3ActionPerformed(evt);
@@ -304,7 +304,7 @@ public class CreateActorFrame extends javax.swing.JFrame {
                         .addComponent(jLabel6)
                         .addGap(18, 18, 18)
                         .addComponent(jRadioButton1)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 271, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 285, Short.MAX_VALUE)
                         .addComponent(jButton3))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -383,16 +383,19 @@ public class CreateActorFrame extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        this.setVisible(false);
+        this.setVisible(false);        
 	UpdaterFrame currentUpdater = new UpdaterFrame();
 	currentUpdater.setResourcesList(this.getResourcesList());
 	currentUpdater.setMainResourcesList(this.getMainResourcesList());
+        if(this.getUpdater() != null)
+            currentUpdater.setUpdater(this.getUpdater());
+        else
+            currentUpdater.setUpdater(new Updater());
         currentUpdater.setBackFrameText("CreateActorFrame");
 	currentUpdater.setBackFrame(this);
 	currentUpdater.setLocationRelativeTo(null);
-	currentUpdater.setVisible(true);
-	this.setUpdater(new Updater());
-	currentUpdater.setUpdater(this.getUpdater());
+	currentUpdater.setVisible(true);  
+         currentUpdater.fillTable();
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jRadioButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jRadioButton1ActionPerformed
