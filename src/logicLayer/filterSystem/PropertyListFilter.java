c5a63a4de7add5c1855fff47e563f5f05845a815
lujan.emmanuel <lujan.emmanuel@gmail.com>
2011-12-18 18:59:36 +0000

    Modificación en PropertyListFilter

--- src/logicLayer/filterSystem/PropertyListFilter.java
+++ src/logicLayer/filterSystem/PropertyListFilter.java
@@ -21,19 +21,19 @@ public class PropertyListFilter extends Filter {
 		this.setResource(currentResource);
 	}
 
-	public boolean eval(Object a) {
-		Resource r1 = (Resource) a;
+	public boolean eval(Object a) {		
 		Vector<Resource> elements = ((Vector<Resource>) a);
-		Resource r2 = (Resource) this.getResource();
-		String id = "";
-		String key = "";
-		String value = "";
+		Resource r1 = (Resource) this.getResource();
+		Resource r2 = null;
+		String id1 = r1.getResId();
+		String id2 = "";
+		String key = this.getKey().toString();
+		String value = this.getValue().toString();
 		int n = elements.size();
 		for (int i = 0; i < n; i++) {
-			id = elements.elementAt(i).getResId();
-			key = this.getKey().toString();
-			value = this.getValue().toString();
-			if ((r2.getResId().equals(id)) && r1.getProperty(key).equals(value))
+			r2 = elements.elementAt(i);
+			id2 = r2.getResId();
+			if ((id1.equals(id2)) && r2.getProperty(key).equals(value))
 				return true;
 		}
 		return false;
